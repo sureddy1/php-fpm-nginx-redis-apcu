f6b4f8d0797cae3bee4610acd04d825faafdaa49
Srikanth Sureddy <srsuredd@microsoft.com>
2020-07-28 16:48:17 -0500

    Check and create php-fpm sock

--- init_container.sh
+++ init_container.sh
@@ -5,6 +5,14 @@
 echo "Starting SSH ..."
 service ssh start
 
+echo "INFO: creating /var/run/php/php-fpm.sock ..."
+test -e /var/run/php/php-fpm.sock && rm -f /var/run/php/php-fpm.sock
+mkdir -p /var/run/php
+touch /var/run/php/php-fpm.sock
+
+chown -R www-data:www-data /var/run/php/php-fpm.sock
+chmod 777 /var/run/php-php-fpm.sock
+
 echo "Starting nginx"
 service nginx start
 
